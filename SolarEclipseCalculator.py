6d5ff2a3a88c90e2c67f1beb2c7867879ac5e8b0
Tyler Allen <tylera277@gmail.com>
2022-01-11 08:35:48 -0500

    calculator made more accurate

--- SolarEclipseCalculator.py
+++ SolarEclipseCalculator.py
@@ -1,8 +1,10 @@
 import numpy as np
 import math
 
+import mpmath as mp
+
 from sideFunctions import time2Julian, earth2Sun, sun2Moon
-from sideFunctions import leapYearAdjuster
+from sideFunctions import leapYearAdjuster, r
 from angleCalculations import AngleCalculations as angleCalc
 
 
@@ -33,11 +35,12 @@ class SolarEclipseCalculator:
 
     def DetermineTimes(self):
 
+        mp.dps = 25
+        months_storage = []
         lowerMonthLimit, upperMonthLimit = self.MonthLimits()
 
         # this gets the position vectors of the stellar bodies that we are using
         from jplephem.spk import SPK
-        kernel = SPK.open('de440.bsp')
 
         possibleSolarEclipseDates = np.zeros((13, 1000))
 
@@ -52,7 +55,7 @@ class SolarEclipseCalculator:
                 dayUpperRange = leapYearAdjuster(TestYear, TestMonth)
 
                 for TestDay in range(1, dayUpperRange, 1):
-                #for TestDay in range(1, 15, 1):
+                #for TestDay in range(14, 15, 1):
                     for TestHour in range(1, 24, 1):
                         for TestMinute in range(1, 60, self.minuteSteps):
 
@@ -76,84 +79,90 @@ class SolarEclipseCalculator:
                             zMoonCent = positionMoon[2]
 
                             # radius of the respective body (km)
-                            r_earth = 6378
+                            r_earth = 6563
+                            #r_earth = 13000
                             r_moon = 1737
                             r_sun = 696000
 
                             # For the sun
-                            sun_x_point1 = r_sun * math.sin(90-math.atan(yMoonCent/xMoonCent))
-                            sun_y_point1 = -r_sun * math.cos(90-math.atan(yMoonCent/xMoonCent))
+                            sun_x_point1 = r_sun * math.sin(r(math.atan(r(yMoonCent / xMoonCent))))
+                            sun_y_point1 = -r_sun * math.cos(r(math.atan(r(yMoonCent/xMoonCent))))
 
-                            sun_x_point2 = -r_sun * math.sin(90-math.atan(yMoonCent/xMoonCent))
-                            sun_y_point2 = r_sun * math.cos(90-math.atan(yMoonCent/xMoonCent))
+                            sun_x_point2 = -r_sun * math.sin(r(math.atan(r(yMoonCent/xMoonCent))))
+                            sun_y_point2 = r_sun * math.cos(r(math.atan(r(yMoonCent/xMoonCent))))
 
                             sun_z_pointT = r_sun
                             sun_z_pointB = -r_sun
 
                             # For the moon
-                            moon_x_point1 = xMoonCent + r_moon * math.sin(90-math.atan(yMoonCent/xMoonCent))
-                            moon_y_point1 = yMoonCent + -r_moon * math.cos(90-math.atan(yMoonCent/xMoonCent))
+                            moon_x_point1 = xMoonCent + r_moon * math.sin(r(math.atan(r(yMoonCent/xMoonCent))))
+                            moon_y_point1 = yMoonCent + -r_moon * math.cos(r(math.atan(r(yMoonCent/xMoonCent))))
 
-                            moon_x_point2 = xMoonCent + -r_moon * math.sin(90 - math.atan(yMoonCent / xMoonCent))
-                            moon_y_point2 = yMoonCent + r_moon * math.cos(90 - math.atan(yMoonCent / xMoonCent))
+                            moon_x_point2 = xMoonCent + -r_moon * math.sin(r(math.atan(r(yMoonCent / xMoonCent))))
+                            moon_y_point2 = yMoonCent + r_moon * math.cos(r(math.atan(r(yMoonCent / xMoonCent))))
 
                             moon_z_pointT = r_moon + zMoonCent
                             moon_z_pointB = -r_moon + zMoonCent
 
                             # For the earth
-                            earth_x_point1 = xEarthCent + r_earth * math.sin(90 - math.atan(yEarthCent / xEarthCent))
-                            earth_y_point1 = yEarthCent + -r_earth * math.cos(90 - math.atan(yEarthCent / xEarthCent))
+                            earth_x_point1 = xEarthCent + r_earth * math.sin(r(math.atan(r(yEarthCent / xEarthCent))))
+                            earth_y_point1 = yEarthCent + -r_earth * math.cos(r(math.atan(r(yEarthCent / xEarthCent))))
 
-                            earth_x_point2 = xEarthCent + -r_earth * math.sin(90 - math.atan(yEarthCent / xEarthCent))
-                            earth_y_point2 = yEarthCent + r_earth * math.cos(90 - math.atan(yEarthCent / xEarthCent))
+                            earth_x_point2 = xEarthCent + -r_earth * math.sin(r(math.atan(r(yEarthCent / xEarthCent))))
+                            earth_y_point2 = yEarthCent + r_earth * math.cos(r(math.atan(r(yEarthCent / xEarthCent))))
 
                             earth_z_pointT = r_earth + zEarthCent
                             earth_z_pointB = -r_earth + zEarthCent
 
                             # ####################################
 
-                            sun2moon_point1_1 = math.atan(
-                                (moon_y_point1 - sun_y_point1) / (moon_x_point1 - sun_x_point1))
-                            sun2earth_point1_1 = math.atan(
-                                (earth_y_point1 - sun_y_point1) / (earth_x_point1 - sun_x_point1))
+                            sun2moon_point1_1 = math.atan(r(
+                                (moon_y_point1 - sun_y_point1) / (moon_x_point1 - sun_x_point1)))
+                            sun2earth_point1_1 = math.atan(r(
+                                (earth_y_point1 - sun_y_point1) / (earth_x_point1 - sun_x_point1)))
 
-                            sun2moon_point2_2 = math.atan(
-                                (moon_y_point2 - sun_y_point2) / (moon_x_point2 - sun_x_point2))
-                            sun2earth_point2_2 = math.atan(
-                                (earth_y_point2 - sun_y_point2) / (earth_x_point2 - sun_x_point2))
+                            sun2moon_point2_2 = math.atan(r(
+                                (moon_y_point2 - sun_y_point2) / (moon_x_point2 - sun_x_point2)))
+                            sun2earth_point2_2 = math.atan(r(
+                                (earth_y_point2 - sun_y_point2) / (earth_x_point2 - sun_x_point2)))
 
-                            sun2moon_point1_2 = math.atan(
-                                (moon_y_point2 - sun_y_point1) / (moon_x_point2 - sun_x_point1))
-                            sun2earth_point1_2 = math.atan(
-                                (earth_y_point2 - sun_y_point1) / (earth_x_point2 - sun_x_point1))
+                            sun2moon_point1_2 = math.atan(r(
+                                (moon_y_point2 - sun_y_point1) / (moon_x_point2 - sun_x_point1)))
+                            sun2earth_point1_2 = math.atan(r(
+                                (earth_y_point2 - sun_y_point1) / (earth_x_point2 - sun_x_point1)))
 
-                            sun2moon_point2_1 = math.atan(
-                                (moon_y_point1 - sun_y_point2) / (moon_x_point1 - sun_x_point2))
-                            sun2earth_point2_1 = math.atan(
-                                (earth_y_point1 - sun_y_point2) / (earth_x_point1 - sun_x_point2))
+                            sun2moon_point2_1 = math.atan(r(
+                                (moon_y_point1 - sun_y_point2) / (moon_x_point1 - sun_x_point2)))
+                            sun2earth_point2_1 = math.atan(r(
+                                (earth_y_point1 - sun_y_point2) / (earth_x_point1 - sun_x_point2)))
 
                             moon_cent = (xMoonCent**2+yMoonCent**2+zMoonCent**2)**(1.0/2.0)
-                            earth_cent = (xEarthCent ** 2 + yEarthCent ** 2+zEarthCent**2) ** (1.0 / 2.0)
+                            earth_cent = (xEarthCent ** 2 + yEarthCent ** 2 + zEarthCent**2) ** (1.0 / 2.0)
 
-                            sun2moon_point_B2B = math.atan((moon_z_pointB - sun_z_pointB)/(moon_cent))
-                            sun2earth_point_B2B = math.atan((earth_z_pointB - sun_z_pointB)/(earth_cent))
+                            sun2moon_point_B2B = math.atan(r((moon_z_pointB - sun_z_pointB)/(moon_cent)))
+                            sun2earth_point_B2B = math.atan(r((earth_z_pointB - sun_z_pointB)/(earth_cent)))
 
-                            sun2moon_point_T2T = math.atan((moon_z_pointT - sun_z_pointT) / (moon_cent))
-                            sun2earth_point_T2T = math.atan((earth_z_pointT - sun_z_pointT) / (earth_cent))
+                            sun2moon_point_T2T = math.atan(r((moon_z_pointT - sun_z_pointT) / (moon_cent)))
+                            sun2earth_point_T2T = math.atan(r((earth_z_pointT - sun_z_pointT) / (earth_cent)))
 
                             # X & Y alignment checker
-                            if ((sun2earth_point1_1<=sun2moon_point1_1<=sun2earth_point1_2) or \
-                                    (sun2earth_point2_1<=sun2moon_point2_2<=sun2earth_point2_2)) or \
-                                    ((sun2earth_point1_1 >= sun2moon_point1_1 >= sun2earth_point1_2) or \
-                                     (sun2earth_point2_1 >= sun2moon_point2_2 >= sun2earth_point2_2)):
+                            #print('car:',TestHour, sun2moon_point1_1, sun2earth_point1_2-sun2moon_point1_1)
+                            if ((sun2earth_point1_1<=sun2moon_point1_1) and (sun2moon_point1_1<=sun2earth_point1_2)) or \
+                                ((sun2earth_point1_1>=sun2moon_point1_1) and (sun2moon_point1_1>=sun2earth_point1_2)):
 
                                     # Z-axis alignment checker
                                     if ((sun2moon_point_B2B <= sun2earth_point_B2B) and (sun2moon_point_T2T >= sun2earth_point_T2T)) \
                                             or \
                                             ((sun2moon_point_B2B >= sun2earth_point_B2B) and (sun2moon_point_T2T <= sun2earth_point_T2T)):
 
-                                        print(TestYear, TestMonth, TestDay, TestHour, TestMinute)
-                                        possibleSolarEclipseDates[TestMonth][counter] = (julianTime)
-                                        counter += 1
+                                        # Selecting only solar eclipses; where moon is closer to sun than earth
+                                        if moon_cent < earth_cent:
+                                            print(TestYear, TestMonth, TestDay, TestHour, TestMinute)
+                                            possibleSolarEclipseDates[TestMonth][counter] = (julianTime)
+                                            if TestMonth in months_storage:
+                                                pass
+                                            else:
+                                                months_storage.append(TestMonth)
+                                            counter += 1
 
-        return possibleSolarEclipseDates
+        return possibleSolarEclipseDates, months_storage
